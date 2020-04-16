c4f037f9dbf060cc232b66f061979ed9e19ce04c
Simon Ganz <simong@gmail.com>
2020-04-15 22:37:46 -0700

    Adding stub function, learning TS/ESlint

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
 const {program} = require('commander');
 const pjson = require('./package.json');
 program.version(pjson.version);
+
+program.parse(process.argv);
+
+/** Function to parse paragraph element and convert to Markdown
+ * @param  {object} jsonElement From "paragraph" field
+ * @return {string} Markdown as string
+ */
+function parseParagraph(jsonElement: object): string {
+  return '## Heading';
+}
