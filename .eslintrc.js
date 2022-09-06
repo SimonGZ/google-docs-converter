9743e9d7b4aa91dcd45cf369e8d3aadfde243363
SimonGZ <dev@simonganz.com>
2022-09-06 10:07:04 -0700

    Removing Google ESlint. Too bossy and annoying.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es6': true,
-  },
-  'extends': [
-    'google',
-  ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
-  },
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 2018,
-  },
-  'plugins': [
-    '@typescript-eslint',
-  ],
-  'rules': {
-  },
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es6': true,
+    },
+    'extends': [
+    ],
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly',
+    },
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 2018,
+    },
+    'plugins': [
+        '@typescript-eslint',
+    ],
+    'rules': {
+        'object-curly-spacing': [2, 'always'],
+    },
 };
